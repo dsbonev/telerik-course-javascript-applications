743dade0598990aec6e94a4dc6ac687df51ea24f
Dimitar Bonev <dsbonev@gmail.com>
2013-07-02 22:07:15 +0300

    render combat units

--- 06.exam/js/models.js
+++ 06.exam/js/models.js
@@ -3,26 +3,55 @@
 var models = (function () {
   'use strict';
 
-  function Ranger() {
-
+  function Unit(position, attack, armor, hitPoints, range, speed) {
+    this.position = position;
+    this.attack = attack;
+    this.armor = armor;
+    this.hitPoints = hitPoints;
+    this.range = range;
+    this.position = position;
   }
 
-  function Warrior() {
-
+  function Ranger(position) {
+    Unit.apply(this, [position, 8, 3, 15, 1, 2]);
   }
+
+  mixin(Ranger.prototype, {
+    toDOM: function () {
+      var el = $('<div class="unit ranger">');
+
+      return el;
+    }
+  });
+
+  function Warrior(position) {
+    Unit.apply(this, [position, 8, 1, 10, 3, 1]);
+  }
+
+  mixin(Warrior.prototype, {
+    toDOM: function () {
+      var el = $('<div class="unit warrior">');
+
+      return el;
+    }
+  });
 
   function Player() {
 
   }
 
-  function Field(players) {
+  function Field(player1, player2) {
     this.horizontalCellCount = 9;
     this.verticalCellCount = 9;
+    this.player1 = player1;
+    this.player2 = player2;
   }
 
   mixin(Field.prototype, {
     toDOM: function () {
       var el = $('<table id=game_field>');
+
+
 
       for (var i = 0; i < this.verticalCellCount; i += 1) {
         var rowEl = $('<tr>');
@@ -33,6 +62,45 @@ var models = (function () {
 
         rowEl.appendTo(el);
       }
+
+      //player1
+      var rangerRowPositions = [1, 3, 5, 7];
+      rangerRowPositions.forEach(function (rowPos) {
+        el.find('tr:nth-of-type(' + (rowPos + 1) + ')')
+          .children()
+          .first()
+            .append(new Ranger({x: 0, y: rowPos}).toDOM().addClass('player1'));
+      });
+
+      var warriorRowPositions = [0, 2, 4, 6, 8];
+      warriorRowPositions.forEach(function (rowPos) {
+        el.find('tr:nth-of-type(' + (rowPos + 1) + ')')
+          .children()
+          .eq(1)
+            .append(new Warrior({x: 1, y: rowPos}).toDOM().addClass('player1'));
+      });
+
+      //player2
+      var rangerRowPositions = [1, 3, 5, 7];
+      rangerRowPositions.forEach(function (rowPos) {
+        el.find('tr:nth-of-type(' + (rowPos + 1) + ')')
+          .children()
+          .last()
+            .append(new Ranger({x: 8, y: rowPos}).toDOM().addClass('player2'));
+      });
+
+      var warriorRowPositions = [0, 2, 4, 6, 8];
+      warriorRowPositions.forEach(function (rowPos) {
+        el.find('tr:nth-of-type(' + (rowPos + 1) + ')')
+          .children()
+          .eq(-2)
+            .append(new Warrior({x: 7, y: rowPos}).toDOM().addClass('player2'));
+      });
+
+      var playerNamesEl = $('<tr>');
+      playerNamesEl.append($('<th colspan=4>').text(this.player1));
+      playerNamesEl.append($('<th colspan=4>').text(this.player2));
+      playerNamesEl.prependTo(el);
 
       return el;
     }
